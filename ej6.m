1c343ab80b49a3e0e06f0e954876fd8d8456bea2
jcl <jlepore@itba.edu.ar>
2017-06-22 09:13:39 -0300

    "ej 7"

---
+++ ej6.m
@@ -0,0 +1,3 @@
+load fisheriris;
+training = meas([1:10,51:60,101:150],1)
+    
---
+++ ej7.m
@@ -0,0 +1,25 @@
+load autos.dat
+%scatter(autos(:,2), autos(:,3))
+hold all;
+res = zeros(50,3);
+ksrlin(autos(:,2), autos(:,3))
+Z = smooth(autos(:,2), autos(:,3),5,'lowess')
+subplot(1,2,1)
+plot(autos(:,2), Z )
+subplot(1,2,2)
+res(:,1) = (Z-autos(:,3)).^2.^(1/2);
+Z = smooth(autos(:,2), autos(:,3),10,'lowess')
+subplot(1,2,1)
+plot(autos(:,2), Z )
+subplot(1,2,2)
+res(:,2) = (Z-autos(:,3)).^2.^(1/2);
+Z = smooth(autos(:,2), autos(:,3),30,'lowess')
+subplot(1,2,1)
+plot(autos(:,2), Z )
+subplot(1,2,2)
+res(:,3) = (Z-autos(:,3)).^2.^(1/2);
+sum(res(:,1))
+sum(res(:,2))
+sum(res(:,3))
+
+
