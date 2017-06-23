98ac9f6fdf6ec6f0bc250ef99f89f972a018f3d4
Sebastian <sguido@itba.edu.ar>
2017-06-23 02:43:41 -0300

    asdasd

--- ej1.m
+++ ej1.m
@@ -3,12 +3,15 @@ num = xlsread('provincias.xlsx');
 [coeff, score, latent] = pca(num);
 %%se expresa casi toda la variabilidad de la muestra con solo dos
 %%componentes
-cumsum(latent)./sum(latent);
+cumsum(latent)./sum(latent)
 %% se clasifican la totalidad de los datos
-%% a partir del biplot.. k = 3?
+nClust = [1:9];
 
+plot(nClust,100* cumsum(latent)./sum(latent));
+
+%%se observa que el codo esta en 3, por lo que se haran 3 clusters
 k=3;
-[idx,C] = kmeans(num,6);
+[idx,C] = kmeans(num,3);
 %%analizar si se separaron por region
 
 
--- ej2bis3.m
+++ ej2bis3.m
@@ -3,14 +3,17 @@ X = meas;
 Y = species;
 
 
+training  = meas([1:33,51:83,101:133],:);
+trainingspecies  = species([1:33,51:83,101:133],:);
+testing = meas ([34:50,84:100,134:150],:);
 
 SVMModels = cell(3,1);
 classes = unique(Y);
 rng(1); % For reproducibility
 
 for j = 1:numel(classes);
-    indx = strcmp(Y,classes(j)); % Create binary classes for each classifier
-    SVMModels{j} = fitcsvm(X,indx,'ClassNames',[false true],'Standardize',true,...
+    indx = strcmp(trainingspecies,classes(j)); % Create binary classes for each classifier
+    SVMModels{j} = fitcsvm(training,indx,'ClassNames',[false true],'Standardize',true,...
         'KernelFunction','rbf','BoxConstraint',1);
 end
 
@@ -19,10 +22,10 @@ end
 %     min(X(:,2)):d:max(X(:,2)));
 % xGrid = [x1Grid(:),x2Grid(:)];
 % N = size();
- Scores = zeros(150,numel(classes));
+ Scores = zeros(51,numel(classes));
 
-for j = 1:numel(classes);
-    [~,score] = predict(SVMModels{j},meas);
+for j = 1:numel(classes)
+    [~,score] = predict(SVMModels{j},testing);
     Scores(:,j) = score(:,2); % Second column contains positive-class scores
 end
 
